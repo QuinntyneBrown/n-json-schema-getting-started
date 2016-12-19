f3b1200265f5fdf6d0f38dc638b0e25d361a9fb3
Quinntyne Brown <quinntynebrown@gmail.com>
2016-12-19 01:02:34 -0500

    initial code commi

---
+++ Person.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace n_json_schema_getting_started
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.TypeScript;
+using System;
+
+namespace n_json_schema_getting_started
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var schema = JsonSchema4.FromType<Person>();            
+            var generator = new TypeScriptGenerator(schema);
+            var file = generator.GenerateFile();
+            Console.Write(file);
+            Console.ReadLine();
+        }
+
+        public static void InvalidJSON()
+        {
+            var schema = JsonSchema4.FromType<Person>();
+            var schemaData = schema.ToJson();
+            var invalidJSON = JsonConvert.SerializeObject(new { FirstName = "Quinn" });
+            var errors = schema.Validate(invalidJSON);
+
+            foreach (var error in errors)
+                Console.WriteLine(error.Path + ": " + error.Kind);
+
+            schema = JsonSchema4.FromJson(schemaData);
+            Console.ReadLine();
+        }
+    }
+}
